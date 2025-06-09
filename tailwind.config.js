58d9963d41c5f6ce90d90825cf7a0b370d3a23af
Danielle Sherman <76003473+dannisherman@users.noreply.github.com>
2025-06-09 17:53:05 +0000

    Fix path import for Vite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        background: 'hsl(var(--background) / <alpha-value>)',
+      },
+      textColor: {
+        foreground: 'hsl(var(--foreground) / <alpha-value>)',
+      },
+      borderColor: {
+        border: 'hsl(var(--border) / <alpha-value>)',
+      },
+      ringColor: {
+        ring: 'hsl(var(--ring) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 }
-
-
