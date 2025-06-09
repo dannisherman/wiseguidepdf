fdcebe27ac4f712c4c4e5320bb3e430b1c76cfa2
Danielle Sherman <76003473+dannisherman@users.noreply.github.com>
2025-06-09 16:01:49 +0000

    Fix path import for Vite config

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import EmailSignup from './components/EmailSignup';
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/signup" element={<EmailSignup />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
