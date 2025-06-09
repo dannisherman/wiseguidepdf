9ea5ab3f4639addc13861309581beb6850add984
Danielle Sherman <76003473+dannisherman@users.noreply.github.com>
2025-06-09 17:58:07 +0000

    Fix path import for Vite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundColor: {
+      colors: {
         background: 'hsl(var(--background) / <alpha-value>)',
-      },
-      textColor: {
         foreground: 'hsl(var(--foreground) / <alpha-value>)',
-        'dark-slate': 'hsl(222.2 47.4% 11.2%)', // Customize this HSL to match your intent
-      },
-      borderColor: {
-        border: 'hsl(var(--border) / <alpha-value>)',
+        'card-foreground': 'hsl(var(--card-foreground) / <alpha-value>)',
+        'dark-slate': '#1e293b', // <- Replace with your desired hex if different
+        'royal-blue': '#4169e1',
       },
       ringColor: {
-        ring: 'hsl(var(--ring) / <alpha-value>)',
-      },
+        'royal-blue': '#4169e1',
+      }
     },
   },
   plugins: [],
